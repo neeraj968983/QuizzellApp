e4cfc7c934365bb7a43f4fc62afb0c3f8dc2a7c0
neeraj968983 <neeraj968983@gmail.com>
2022-05-07 06:46:46 +0530

    all files

--- app/src/main/java/com/example/quizellapp/MentorDashboard.kt
+++ app/src/main/java/com/example/quizellapp/MentorDashboard.kt
@@ -1,17 +1,18 @@
 package com.example.quizellapp
 
+import android.app.Dialog
 import android.app.PendingIntent
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.telephony.SmsManager
 import android.view.MenuItem
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.marginLeft
@@ -64,7 +65,9 @@ class MentorDashboard : AppCompatActivity() {
                     intent.putExtra("usernameFromLogin",i.getStringExtra("usernameFromLogin"))
                     startActivity(intent)
                 }
-                R.id.AccountSummary -> Toast.makeText(applicationContext, "Account Summary is under progress", Toast.LENGTH_LONG).show()
+                R.id.AccountSummary -> {
+                    accountSummaryDialog()
+                }
                 R.id.Privacy -> {
                     var intent: Intent = Intent(this,PrivacyMentor::class.java)
                     intent.putExtra("usernameFromLogin",i.getStringExtra("usernameFromLogin"))
@@ -139,5 +142,35 @@ class MentorDashboard : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
 
+    }
+    fun accountSummaryDialog(){
+        var dialog:Dialog = Dialog(this)
+        dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.account_summary_dialogbox)
+
+        var exit:Button = dialog.findViewById(R.id.Exit)
+        var withdraw:Button = dialog.findViewById(R.id.Withdraw)
+        var info:TextView = dialog.findViewById(R.id.Information)
+
+        exit.setOnClickListener {
+            dialog.dismiss()
+        }
+        withdraw.setOnClickListener {
+            Toast.makeText(this, "Currently payment option not available!",Toast.LENGTH_SHORT).show()
+        }
+        info.setOnClickListener {
+            var infoDialog = Dialog(this)
+            infoDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            infoDialog.setContentView(R.layout.infopage_account_summary)
+
+            var OK:TextView = infoDialog.findViewById(R.id.OK)
+
+            OK.setOnClickListener {
+                infoDialog.dismiss()
+            }
+            infoDialog.show()
+
+        }
+        dialog.show()
     }
 }
